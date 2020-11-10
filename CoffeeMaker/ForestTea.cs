c3ad8479680510340c59160369701476b1cd6a1c
PhilipGeil <pgj.philip@gmail.com>
2020-11-10 12:05:28 +0100

    Part two

---
+++ CoffeeMaker/ForestTea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMaker
+{
+    /// <summary>
+    /// Represents ForestTea, which obviously inherits from Beans - Or not.. - The idea is right though, it's just bad naming..
+    /// </summary>
+    class ForestTea : Beans
+    {
+        /// <summary>
+        /// Default constructor - takes from it's base class.
+        /// </summary>
+        /// <param name="amount"></param>
+        ForestTea(double amount) : base(amount) { }
+    }
+}
